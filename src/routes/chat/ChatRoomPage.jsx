0989dd7464975ca82183b568923563ccabfdbe44
yeri <rlafl7942@naver.com>
2024-08-04 17:25:02 +0900

    feat: ChatPartnerInfo, ChatSender/ReceiverComponent 추가

---
+++ src/routes/chat/ChatRoomPage.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+// assets
+import consulting from '../../assets/consulting.png';
+
+export default function ChatRoomPage() {
+	return (
+		<>
+			<div className="flex items-center justify-center h-16 p-5 font-bold">
+				채팅
+			</div>
+			<ChatPartnerInfo />
+			<div className="grid w-full px-5 mt-6">
+				<ChatSenderComponent />
+				<ChatReceiverComponent />
+			</div>
+		</>
+	);
+}
+
+const ChatPartnerInfo = () => {
+	return (
+		<div className="flex items-center justify-between px-5">
+			<div className="flex">
+				<div className="w-12 h-12 bg-gray-300 rounded-full" />
+				<div className="flex flex-col justify-center mx-4">
+					<span className="text-[18px] font-bold">권기현 팀장</span>
+					<span className="text-[13px]">머시기저시기 담당</span>
+				</div>
+			</div>
+			<div className="flex flex-col items-center justify-center">
+				<img src={consulting} className="w-5" />
+				<span className="text-[12px] mt-1">상담 신청하기</span>
+			</div>
+		</div>
+	);
+};
+
+const ChatSenderComponent = () => {
+	return (
+		<div className="flex justify-self-end">
+			<span className="text-[12px] text-[#8F8F8F] mb-2 mr-2 flex items-end">
+				18:22
+			</span>
+			<div className="px-5 py-2 bg-[#D8E2FF] w-fit rounded-[10px] my-2">
+				<span className="text-[17px]">안녕하세요~</span>
+			</div>
+		</div>
+	);
+};
+
+const ChatReceiverComponent = () => {
+	return (
+		<div className="flex flex-end">
+			<div className="px-5 py-2 bg-[#F1F1F1] w-fit rounded-[10px] my-2">
+				<span className="text-[17px]">안녕하세요~ 늦게 답장 지송~</span>
+			</div>
+			<span className="text-[12px] text-[#8F8F8F] mb-2 ml-2 flex items-end">
+				18:22
+			</span>
+		</div>
+	);
+};
