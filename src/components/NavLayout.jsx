e36391bad456eb64e70a45c18bb3a7a7969fd4bd
yeri <rlafl7942@naver.com>
2024-08-05 09:51:25 +0900

    feat: 홈페이지 UI 구현

--- src/components/NavLayout.jsx
+++ src/components/NavLayout.jsx
@@ -4,11 +4,11 @@ import BottomNavigation from './BottomNavigation';
 
 export default function NavLayout() {
 	return (
-    <>
-      <div className="container">
-        <Outlet />
-      </div>
-      <BottomNavigation/>
-    </>
+		<>
+			<div>
+				<Outlet />
+			</div>
+			<BottomNavigation />
+		</>
 	);
 }
