b2818ded346885af148affe2ded2c74ff02b1157
yeri <rlafl7942@naver.com>
2024-08-06 22:31:03 +0900

    feat: 고객일 경우, 캘린더 페이지 헤더 + 버튼 제거

--- src/routes/calendar/CalendarPage.jsx
+++ src/routes/calendar/CalendarPage.jsx
@@ -19,39 +19,49 @@ export default function CalendarPage() {
 		fetchTodaySchedules();
 	}, [value]);
 
-	const fetchTodaySchedules = async() => {
-		const today = moment(value).format("YYYY-MM-DD")
-		try{
+	const fetchTodaySchedules = async () => {
+		const today = moment(value).format('YYYY-MM-DD');
+		try {
 			const response = await getTodaySchedules(id, today, role);
 			setSchedules(response.response);
-		} catch(error){
+		} catch (error) {
 			console.log(error);
 			setSchedules([]);
 		} finally {
 			setIsLoading(false);
 		}
-	}
+	};
 
 	return (
-		<div className="bg-sh-gr-01 h-screen">
-			<div className={`${isAddSchedule ? "opacity-20" : ""} w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600 text-xl font-sans font-bold flex justify-center items-center relative shadow`}>
+		<div className="h-screen bg-sh-gr-01">
+			<div
+				className={`${isAddSchedule ? 'opacity-20' : ''} w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600 text-xl font-sans font-bold flex justify-center items-center relative shadow`}
+			>
 				<div>일정</div>
-				<button
-					className="absolute right-4"
-					onClick={() => setIsAddSchedule(!isAddSchedule)}
-				>
-					+
-				</button>
+				{role === 0 ? (
+					<button
+						className="absolute right-4"
+						onClick={() => setIsAddSchedule(!isAddSchedule)}
+					>
+						+
+					</button>
+				) : null}
 			</div>
-			<AddSchedule id="default-modal" isAddSchedule={isAddSchedule} setIsAddSchedule={setIsAddSchedule}/>
-			<div className={`p-3 ${isAddSchedule ? "opacity-10" : ""}`}>
-				<CustomCalendar value={value} onChange={onChange}/>
+			<AddSchedule
+				id="default-modal"
+				isAddSchedule={isAddSchedule}
+				setIsAddSchedule={setIsAddSchedule}
+			/>
+			<div className={`p-3 ${isAddSchedule ? 'opacity-10' : ''}`}>
+				<CustomCalendar value={value} onChange={onChange} />
 				<div className="mt-6">
-					<p className="px-1 font-black">{moment(value).format('YYYY년 MM월 DD일')}</p>
+					<p className="px-1 font-black">
+						{moment(value).format('YYYY년 MM월 DD일')}
+					</p>
 					{[...schedules].map(schedule => (
 						<Schedule
 							key={schedule.dayTime}
-							dayTime={moment(schedule.dayTime).format("HH:mm")}
+							dayTime={moment(schedule.dayTime).format('HH:mm')}
 							name={schedule.scheduleName}
 							place={schedule.schedulePlace}
 							description={schedule.scheduleDescription}
