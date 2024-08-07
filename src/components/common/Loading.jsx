97893f917805c965746b61a9100282d35743f0ef
yeri <rlafl7942@naver.com>
2024-08-07 13:42:17 +0900

    design: Loading Indicator border style 수정
    solid -> dashed

--- src/components/common/Loading.jsx
+++ src/components/common/Loading.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Loading() {
 	return (
 		<div className="flex items-center justify-center h-[500px]">
-			<div className="w-16 h-16 border-4 border-t-4 border-blue-500 border-solid rounded-full animate-spin" />
+			<div className="w-16 h-16 border-4 border-t-4 border-blue-500 border-dashed rounded-full animate-spin" />
 		</div>
 	);
 }
